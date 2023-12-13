08180782524b42ee546ed3886582c46763682fda
mxmreporting <142613858+mxmreporting@users.noreply.github.com>
2023-12-13 16:03:58 +0530

    Update ad_reporting__ad_report.sql

--- models/ad_reporting__ad_report.sql
+++ models/ad_reporting__ad_report.sql
@@ -1,5 +1,11 @@
 {% set enabled_packages = get_enabled_packages() %}
-{{ config(enabled=is_enabled(enabled_packages)) }}
+{{ config(enabled=is_enabled(enabled_packages),
+    unique_key = ['source_relation','platform','date_day','ad_id','ad_group_id','campaign_id','account_id'],
+    partition_by={
+      "field": "date_day",
+      "data_type": "date"
+    }
+    ) }}
 
 with base as (
 
