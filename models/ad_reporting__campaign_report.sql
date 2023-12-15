0f24dd906fdb900fe4794739072a36c24088fcd4
mxmreporting <142613858+mxmreporting@users.noreply.github.com>
2023-12-15 09:35:34 +0530

    Update ad_reporting__campaign_report.sql

--- models/ad_reporting__campaign_report.sql
+++ models/ad_reporting__campaign_report.sql
@@ -3,7 +3,8 @@
     unique_key = ['source_relation','platform','date_day','campaign_id','account_id'],
     partition_by={
       "field": "date_day",
-      "data_type": "date"
+      "data_type": "date",
+      "granularity": "day"
     }
     ) }}
 
