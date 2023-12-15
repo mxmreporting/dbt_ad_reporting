ac991d7e1d64c3163759d8f824b103398a4fd6cb
mxmreporting <142613858+mxmreporting@users.noreply.github.com>
2023-12-15 09:36:31 +0530

    Update ad_reporting__url_report.sql

--- models/ad_reporting__url_report.sql
+++ models/ad_reporting__url_report.sql
@@ -3,7 +3,8 @@
     unique_key = ['source_relation','platform','date_day','ad_group_id','campaign_id','account_id','base_url','url_host','url_path','utm_campaign','utm_content','utm_medium','utm_source','utm_term'],
     partition_by={
       "field": "date_day",
-      "data_type": "date"
+      "data_type": "date",
+      "granularity": "day"
     }
     ) }}
 
