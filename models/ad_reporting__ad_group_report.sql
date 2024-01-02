6bcef1befd939f4d22eb95ce41ee24ebe6b25f56
mxmreporting <142613858+mxmreporting@users.noreply.github.com>
2024-01-02 15:22:56 +0530

    Update ad_reporting__ad_group_report.sql

--- models/ad_reporting__ad_group_report.sql
+++ models/ad_reporting__ad_group_report.sql
@@ -33,7 +33,33 @@ aggregated as (
 
     from base
     {{ dbt_utils.group_by(9) }}
-)
-
+),
+--Video data integration
+    
+all_data as(
 select *
 from aggregated
+
+union all
+
+
+select
+'' as source_relation,
+date(FORMAT_TIMESTAMP("%F %T", airing_data_aired_at_et, "America/New_York")) AS date_day,
+'video' as platform,
+cast(brand_data_id as string) as account_id,
+brand_data_name as account_name,
+'' as campaign_id,
+'' as campaign_name,
+cast(creative_data_spot_id as string) as ad_group_id,
+spot_data_title_short as ad_group_name,
+sum(CAST(0 as INT64)) as clicks,
+sum(CAST(audience_data_impressions as INT64)) as impressions,
+sum(CAST(airing_data_spend_estimated as FLOAT64)) as spend
+from {{ ref('int_ispot_airings_joined') }}
+group by 1,2,3,4,5,6,7,8,9
+
+)
+select *
+from all_data
+
