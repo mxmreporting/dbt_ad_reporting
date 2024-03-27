9c3b2ae497eb5de6862f8d9593aeca485b15cd67
mxmreporting <142613858+mxmreporting@users.noreply.github.com>
2024-03-27 17:09:21 +0530

    Update ad_reporting__account_report.sql

--- models/ad_reporting__account_report.sql
+++ models/ad_reporting__account_report.sql
@@ -46,7 +46,7 @@ date(airing_data_aired_at_et) AS date_day,
 'video' as  platform ,
 CAST(brand_data_id as string) as account_id,
 brand_data_name as account_name,
-sum(CAST(0 as INT64)) as clicks,
+sum(case when conversion_type="VISIT" then COALESCE(cast(conversion_data_tv_population_data_total as int) , 0 ) else 0 end) as clicks,
 sum(CAST(audience_data_impressions as INT64)) as impressions,
 sum(CAST(airing_data_spend_estimated as FLOAT64)) as spend,
 sum(CAST(0 as FLOAT64)) as conversions
